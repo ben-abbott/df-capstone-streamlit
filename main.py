8f296a0e7cff17e4d0a26d56c7c66e8f734a6fcd
Benjamin Abbott <ben_abbott@outlook.com>
2025-05-14 17:08:03 +0100

    working on main, branching now

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import streamlit as st
 from load import load_data
+from metrics import display_metrics
 
 df = load_data()
 
@@ -7,5 +8,8 @@ df = load_data()
 st.title("My First Streamlit App")
 # Display a simple message
 st.write("Hello, Streamlit!")
+
+display_metrics(df)
+
 
 st.dataframe(df)
---
+++ metrics.py
@@ -0,0 +1,9 @@
+import streamlit as st
+
+
+def get_highest_roc(df):
+    pass
+
+
+def display_metrics(df):
+    pass
